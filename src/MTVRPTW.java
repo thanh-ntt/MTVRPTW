575e3c726366e3c521b1542b4217d43b30de7417
Thanh <truongthanh@u.nus.edu>
2020-12-17 11:39:45 +0700

    Change how solution stats are displayed

--- src/MTVRPTW.java
+++ src/MTVRPTW.java
@@ -9,9 +9,11 @@ import java.util.stream.Stream;
 public class MTVRPTW {
 
     static final Logger logger = Logger.getLogger(MTVRPTW.class.getName());
+    static final boolean SHOW_SOLUTION_STATS = true;
     static final boolean SHOW_ROUTE_STATS = false;
 
     public static void main(String[] args) {
+        StringBuilder logMsg = new StringBuilder();
         SolutionConstructionAlgorithm[] solutionConstructionAlgorithms
                 = {new GreedyAlgorithm(), new SolomonI1Algorithm(), new ClusterRouting(), new ChangsAlgorithm()};
 //        solutionConstructionAlgorithms = Arrays.copyOfRange(solutionConstructionAlgorithms, 1, 2);
@@ -23,11 +25,13 @@ public class MTVRPTW {
         Configurations configs = new Configurations(0, inputDirectory.getPath() + "/parameters.txt");
 
         String[] testSets = inputDirectory.list((dir, name) -> new File(dir, name).isDirectory());
+        Arrays.sort(testSets);
         assert testSets != null;
         int[] cumulativeLength = new int[numAlgorithms + 1];
         for (String testSet : testSets) {
             String testDirectory = inputDirectory + "/" + testSet;
             String[] inputFiles = Objects.requireNonNull(new File(testDirectory).list((dir, name) -> new File(dir, name).isFile()));
+            Arrays.sort(inputFiles);
             List<Route>[][] solutions = new ArrayList[numAlgorithms + 1][inputFiles.length];
             int[][] results = new int[numAlgorithms + 1][inputFiles.length];
             for (int i = 0; i < inputFiles.length; i++) {
@@ -36,17 +40,26 @@ public class MTVRPTW {
                     solutions[j][i] = solutionConstructionAlgorithms[j].run(dataModel);
                     results[j][i] = solutions[j][i].size();
                 }
+                // Local search
                 solutions[results.length - 1][i] = new RelocateAlgorithm().run(solutions[1][i], dataModel);
                 results[results.length - 1][i] = solutions[results.length - 1][i].size();
             }
-            double[] averageResults = new double[results.length];
+            int[] sumSolutionSize = new int[results.length];
             for (int i = 0; i < results.length; i++) {
-                averageResults[i] = Arrays.stream(results[i]).average().orElse(Double.NaN);
+                sumSolutionSize[i] = Arrays.stream(results[i]).sum();
                 cumulativeLength[i] += Arrays.stream(results[i]).sum();
             }
-            logger.info(testSet + ": "
-                    + Arrays.toString(Arrays.stream(averageResults).map(x -> Math.round(x * 100.0) / 100.0).toArray()));
+            logMsg.append(testSet + ":\n");
+            if (SHOW_SOLUTION_STATS) {
+                for (int i = 0; i < results.length; i++) {
+                    logMsg.append(i + ": " + Arrays.toString(results[i]) + "\n");
+                }
+            }
+            logMsg.append("Sum: " + Arrays.toString(sumSolutionSize) + "\n");
+            logMsg.append("Average: " + Arrays.toString(Arrays.stream(sumSolutionSize)
+                    .mapToDouble(x -> Math.round(x * 100.0 / inputFiles.length) / 100.0).toArray()) + "\n\n");
         }
-        logger.info("Cumulative: " + Arrays.toString(cumulativeLength));
+        logMsg.append("Cumulative sum: " + Arrays.toString(cumulativeLength) + "\n");
+        logger.info(logMsg.toString());
     }
 }
